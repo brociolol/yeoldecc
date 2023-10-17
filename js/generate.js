d10b26d7a8c3fb9d8ac25cf972effe708aeb55bc
brociolol <118844723+brociolol@users.noreply.github.com>
2023-10-16 19:33:33 -0500

    Update generate.js
    Added saving throws counter

--- js/generate.js
+++ js/generate.js
@@ -157,6 +157,34 @@
             characterSheetHTML += '<button type="button" class="btn btn-outline-secondary" onclick="modifyHP(\'R\')">Reset</button>';
             characterSheetHTML += '</div></div>';
 
+            characterSheetHTML += '<h6>Death Saving Throws</h6>';
+            characterSheetHTML += '<h7>Success</h7><br>';
+            characterSheetHTML += '<div class="form-check form-check-inline mb-3">';
+            characterSheetHTML += '<input class="form-check-input" type="checkbox" id="inlineCheckbox1" value="option1">';
+            characterSheetHTML += '<label class="form-check-label" for="inlineCheckbox1">1</label>';
+            characterSheetHTML += '</div>';
+            characterSheetHTML += '<div class="form-check form-check-inline">';
+            characterSheetHTML += '<input class="form-check-input" type="checkbox" id="inlineCheckbox2" value="option2">'
+            characterSheetHTML += '<label class="form-check-label" for="inlineCheckbox2">2</label>';
+            characterSheetHTML += '</div>';
+            characterSheetHTML += '<div class="form-check form-check-inline">';
+            characterSheetHTML += '<input class="form-check-input" type="checkbox" id="inlineCheckbox3" value="option3">';
+            characterSheetHTML += '<label class="form-check-label" for="inlineCheckbox3">3</label>';
+            characterSheetHTML += '</div><br>';
+            characterSheetHTML += '<h7>Fail</h7><br>';
+            characterSheetHTML += '<div class="form-check form-check-inline mb-5">';
+            characterSheetHTML += '<input class="form-check-input" type="checkbox" id="inlineCheckbox1" value="option1">';
+            characterSheetHTML += '<label class="form-check-label" for="inlineCheckbox1">1</label>';
+            characterSheetHTML += '</div>';
+            characterSheetHTML += '<div class="form-check form-check-inline">';
+            characterSheetHTML += '<input class="form-check-input" type="checkbox" id="inlineCheckbox2" value="option2">'
+            characterSheetHTML += '<label class="form-check-label" for="inlineCheckbox2">2</label>';
+            characterSheetHTML += '</div>';
+            characterSheetHTML += '<div class="form-check form-check-inline">';
+            characterSheetHTML += '<input class="form-check-input" type="checkbox" id="inlineCheckbox3" value="option3">';
+            characterSheetHTML += '<label class="form-check-label" for="inlineCheckbox3">3</label>';
+            characterSheetHTML += '</div>';
+
             for(let as = 0; as < characterObject.abilityscores.length; as++) {
               let abilityName = characterObject.abilityscores[as].name;
               let name = abilityName.toLowerCase();
@@ -513,10 +541,10 @@
           console.log('loop started');
           let raceModInfo = raceModifiers.find(raceModifiers => raceModifiers.name === abilityScores[q].name);
           if (raceModInfo) {
-            console.log(abilityScores[q].name + ' ' + abilityScores[q].score);
-            let newScore = raceModInfo.value + abilityScores[q].score;
+            //console.log(abilityScores[q].name + ' ' + abilityScores[q].score);
+            let newScore = parseInt(raceModInfo.value) + abilityScores[q].score;
             abilityScores[q]["score"] = newScore;
-            console.log(abilityScores[q].name + ' ' + abilityScores[q].score);
+            //console.log(abilityScores[q].name + ' ' + abilityScores[q].score);
             
           }
           var modNumber = Math.floor((parseInt(abilityScores[q]["score"]) - 10) / 2);
